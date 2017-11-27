e95b391d080212a5d5a8e12d562dd713ca588b3c
mitutee <mitutee@gmail.com>
2017-11-27 07:41:21 +0200

    Extend Edge with edgeKeys

--- BeeGraph/Core.hs
+++ BeeGraph/Core.hs
@@ -1,22 +1,18 @@
-module Core (
-    Node(..),
-    Edge(..),
-    Graph(..),
-    dialog
-) where
+module Core where
 
 import Data.List
 
-data Node = Node { nodeId :: Int, nodeBody :: String, nodeEdges :: [Edge] } deriving (Show)
+data Node = Node { nodeId :: Int, nodeBody :: String, nodeEdges :: [Edge] } deriving (Read, Show)
 
-data Edge = Edge {edgeId :: Int, edgeBody :: String, edgeNodeId :: Int } deriving (Show)
+data Edge = Edge {edgeId :: Int, edgeBody :: String, edgeKeys :: [String], edgeNodeId :: Int } deriving (Read, Show)
 
-data Graph = Graph { nodes :: [Node] } deriving (Show)
+data Graph = Graph { nodes :: [Node] } deriving (Read, Show)
 
 dialog graph nodeId' userResponse = 
     let findNode = find (\n -> (nodeId n) == nodeId')
-        findEdge = find (\e -> edgeBody e == userResponse)
+        findEdge = find (\e -> containsKey $ edgeKeys e)
+        containsKey = elem userResponse
         currentNode = findNode $ nodes graph
-        edges = fmap nodeEdges currentNode                
+        edges = fmap nodeEdges currentNode             
     in  edges >>= findEdge
     
--- BeeGraph/Io.hs
+++ BeeGraph/Io.hs
@@ -4,17 +4,17 @@ import Core
 
 testNodes = [
         Node { nodeId = 0, nodeBody = "Home", nodeEdges = [
-                    Edge { edgeId = 0, edgeBody = "Show help", edgeNodeId = 2},
-                    Edge { edgeId = 1, edgeBody = "Tell me a joke, please!", edgeNodeId = 1}
+                    Edge { edgeId = 0, edgeBody = "Show help", edgeKeys = ["1", "help"], edgeNodeId = 2},
+                    Edge { edgeId = 1, edgeBody = "Tell me a joke, please!", edgeKeys = ["2", "joke"], edgeNodeId = 1}
                 ]},
         Node { nodeId = 1, nodeBody = "Joke", nodeEdges = [
-                    Edge {edgeId = 2, edgeBody = "Go home", edgeNodeId = 0}, 
-                    Edge {edgeId = 3, edgeBody = "Tell me another joke!", edgeNodeId = 1}
+                    Edge {edgeId = 2, edgeBody = "Go home", edgeKeys = ["0", "home"], edgeNodeId = 0}, 
+                    Edge {edgeId = 3, edgeBody = "Tell me another joke!", edgeKeys = ["2", "joke"], edgeNodeId = 1}
                 ]},
 
         Node { nodeId = 2, nodeBody = "Help", nodeEdges = [
-            Edge {edgeId = 2, edgeBody = "Go home", edgeNodeId = 0}, 
-            Edge {edgeId = 3, edgeBody = "Tell me another joke!", edgeNodeId = 1}
+            Edge {edgeId = 2, edgeBody = "Go home", edgeKeys = ["0", "home"], edgeNodeId = 0}, 
+            Edge {edgeId = 3, edgeBody = "Tell me another joke!", edgeKeys = ["2", "joke"], edgeNodeId = 1}
         ]}
     ]
 
